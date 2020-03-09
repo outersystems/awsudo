b29daded54a469761cf37f1a6f4a08d922397ef7
Cell <>
2020-03-09 10:17:34 +0100

    Baby steps...

--- awsudo/run.py
+++ awsudo/run.py
@@ -1,7 +1,13 @@
 #!/bin/env python3
 import awsudo.main
+import os
+import pathlib
+import json
 
-awsudo.main.main()
+cache_dir = "~/.aws/awsudo/cache/"
+cache_file = "session.json"
+
+#awsudo.main.main()
 
 # mkdir ~/.aws/awsudo
 # if ~/.aws/awsudo/session.json or expired then
@@ -12,3 +18,25 @@ awsudo.main.main()
 # else
 #   No idea...
 
+
+cache_dir_path = os.path.expanduser(cache_dir)
+pathlib.Path(cache_dir_path).mkdir(parents=True, exist_ok=True)
+
+session_creds = {}
+
+if os.path.isfile(cache_dir_path + cache_file):
+    print("Yes")
+    with open(cache_dir_path + cache_file) as json_file:
+        data = json.load(json_file)
+        session_creds['field1'] = data['field1']
+        
+else:
+    print("No")
+
+# If outdated - 1h:
+#   Query AWS
+
+
+
+# import json
+# print(json.dumps({'4': 5, '6': 7}, sort_keys=True, indent=4))
