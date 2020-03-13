6ec04a24677bee8d92eb4b49111e42de16d3f495
Cell <>
2020-03-13 23:07:41 +0100

    TRY

--- awsudo/run.py
+++ awsudo/run.py
@@ -3,9 +3,16 @@ import awsudo.main
 import os
 import pathlib
 import json
+import datetime
+import dateutil.parser
+import pytz
+import boto3
+
+
 
 cache_dir = "~/.aws/awsudo/cache/"
 cache_file = "session.json"
+session_creds = {}
 
 #awsudo.main.main()
 
@@ -18,20 +25,47 @@ cache_file = "session.json"
 # else
 #   No idea...
 
+# class session_creds:
+#   def __init__(self, data):
+#       self.data = data
 
-cache_dir_path = os.path.expanduser(cache_dir)
-pathlib.Path(cache_dir_path).mkdir(parents=True, exist_ok=True)
+def fetch_session_token():
+    import getpass
+    import configparser
 
-session_creds = {}
+    config = configparser.ConfigParser()
+    config.read([os.path.expanduser("~/.aws/config")])
+    durationSeconds = config.get("default", 'duration_seconds')
+    mfaSerial = config.get("default", 'mfa_serial')
 
-if os.path.isfile(cache_dir_path + cache_file):
-    print("Yes")
-    with open(cache_dir_path + cache_file) as json_file:
-        data = json.load(json_file)
-        session_creds['field1'] = data['field1']
+    sts_client = boto3.client('sts')
+
+    mfaToken = getpass.getpass(prompt="Enter MFa token: ")
+
+    sessionCreds = sts_client.get_session_token(durationSeconds, mfaSerial, mfaToken)
+
+def main():
+    cache_dir_path = os.path.expanduser(cache_dir)
+    pathlib.Path(cache_dir_path).mkdir(parents=True, exist_ok=True)
+
+    # Load session creds
+    if os.path.isfile(cache_dir_path + cache_file):
+        with open(cache_dir_path + cache_file) as json_file:
+            session_creds = json.load(json_file)
+
+    # Should we refresh session creds?
+    if data.has_key('Expiration')
+        expiration_utc = dateutil.parser.isoparse(data['Expiration'])
+        now_utc = pytz.utc.localize(datetime.datetime.utcnow())
+        max_accepted_timedelta = datetime.timedelta(hours=1)
         
-else:
-    print("No")
+        if (expiration_utc - now_utc) <= max_accepted_timedelta:
+            #renew
+    else:
+        #renew        
+
+    # here session creds are fine.
+
 
 # If outdated - 1h:
 #   Query AWS
@@ -40,3 +74,6 @@ else:
 
 # import json
 # print(json.dumps({'4': 5, '6': 7}, sort_keys=True, indent=4))
+
+if __name__ == '__main__':
+    main()
